d4c11c9f4484ba63330dd63ac2472d67d4be10e4
Oliver <oliverkmiec24@gmail.com>
2020-10-18 18:18:21 -0400

    update

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -1,27 +1,39 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class admin extends BaseController
 {
-	public function index() {
-		$this->renderTemplate('admin/index.phtml');
+	public function index()
+	{
+		$itemModel = model('App\Models\ItemModel');
+		$data = [
+			"categories" => $itemModel->getCategories()
+		];
+
+		$this->renderTemplateView(view('admin/index.phtml', $data));
 	}
 
-	public function pages($page='index')
+	public function pages($page = 'index')
 	{
+		$itemModel = model('App\Models\ItemModel');
+		$data = [
+			"categories" => $itemModel->getCategories()
+		];
+
 		switch ($page) {
 			case 'manage_categories':
-				$this->renderTemplate('admin/manage_categories.phtml');
+				$this->renderTemplateView(view('admin/manage_categories.phtml', $data));
 				break;
 			case 'manage_products':
-				$this->renderTemplate('admin/manage_products.phtml');
+				$this->renderTemplateView(view('admin/manage_products.phtml', $data));
 				break;
 			case 'change_password':
-				$this->renderTemplate('admin/change_password.phtml');
+				$this->renderTemplateView(view('admin/change_password.phtml', $data));
 				break;
 			default:
 				echo "page not found";
 				break;
 		}
 	}
-
 }
