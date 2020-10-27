4b2c46f1bbebbb16ff0b33434f6b833d8b2bda45
Oliver <oliverkmiec24@gmail.com>
2020-10-27 19:47:59 -0400

    home items

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,13 +2,16 @@
 
 namespace App\Controllers;
 
+use App\Models\ItemModel;
+
 class Home extends BaseController
 {
 	public function index()
 	{
 		$itemModel = model('App\Models\ItemModel');
 		$data = [
-			"categories" => $itemModel->getCategories()
+			"categories" => $itemModel->getCategories(),
+			"items" => $itemModel->getItems()
 		];
 		return $this->renderTemplateView(view('home/index.phtml', $data));
 	}
